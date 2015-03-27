a0ac48fff011ee5f42b00e482751799586d4924e
Matthew Chupp <mattlchupp@gmail.com>
2015-03-27 14:25:52 -0500

    added ArrayUnsortedList implementation

--- ArrayUnsortedList.java
+++ ArrayUnsortedList.java
@@ -7,14 +7,36 @@ package lists;
 public class ArrayUnsortedList extends List 
     implements UnsortedListInterface {
 
-    @Override
-    public void add(Object element) {
-        // TODO Auto-generated method stub
-        
+    public ArrayUnsortedList() {
+        super(); 
     }
-
-     
-  
-
+    
+    public ArrayUnsortedList(int origCap) {
+        super(origCap);
+    }
+    
+    /**Adds element to list */
+    public void add(Object element) {
+        
+        if (numElements == list.length) {
+            enlarge(); 
+        } 
+        list[numElements] = element; 
+        numElements++; 
+    }
+    
+    /** Removes an element e from this list such that e.equals(element)
+     * and returns true; if no such element exists, returns false
+     */
+    public boolean remove (Object element) {
+        
+        find(element); 
+        if (found) {
+            list[location] = list[numElements - 1];
+            list[numElements - 1] = null; 
+            numElements--; 
+        }
+        return found; 
+    }
     
 }
